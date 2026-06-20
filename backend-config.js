d7066fc042739fcdc74d6ff1c4b30f77dc6ee891
krp-01 <alexandrucarp43@gmail.com>
2026-06-20 11:59:23 +0300

    Update CLOUD_BACKEND_URL to new Google Apps Script URL

--- backend-config.js
+++ backend-config.js
@@ -1,2 +1,2 @@
 // Pune aici același link Web App de la Google Apps Script ca în portal-pd/assets/cloud-config.js.
-window.CLOUD_BACKEND_URL = "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+window.CLOUD_BACKEND_URL = "https://script.google.com/macros/s/AKfycbzX4zcqUJBphioqAIaqSjHhRuAKkSD8qDDr_x1VFCcYDcEAq3FjkajITq5mD1JTCI1V/exec";
